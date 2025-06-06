1d38de51a58dd2285889533f8801c32f739b65c6
Nathan Dieuveil <nathan304@gmail.com>
2025-06-06 15:51:25 +0100

    nouvelle

--- app.py
+++ app.py
@@ -126,9 +126,10 @@ numerical_features=['montant_credit', 'total_echeance',
 # leasing_filtered.head(2)
 
 #Chargement de la base de données
+#pd.read_csv(path,encoding='utf-8', sep=';')
 @st.cache_data
 def load_data():
-    path = os.path.join("data", "leasing_filtered.csv")  #base_leasing_finale.xlsx
+    path = os.path.join("data", "leasing_filtered_1.csv")  #base_leasing_finale.xlsx
     return  pd.read_csv(path,encoding='utf-8', sep=';') #pd.read_excel(path)
 base_leasing = load_data()
 
